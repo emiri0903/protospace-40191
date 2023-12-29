10b12470b3a340716b292c562772f0fa1a4e755e
emiri0903 <emirintosh@gmail.com>
2023-12-29 18:24:15 +0900

    トップページにリダイレクトされるよう修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -57,5 +57,9 @@ class PrototypesController < ApplicationController
     redirect to prototype_path unless current_user == @prototype.user
   end
 
-
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
