755e5645c1c67fd245e959ab84836e21f82c5338
emiri0903 <emirintosh@gmail.com>
2024-01-04 11:11:38 +0900

    トップページへのリダイレクト

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -54,7 +54,7 @@ class PrototypesController < ApplicationController
   end  
 
   def contributor_confirmation
-    redirect_to prototype_path unless current_user == @prototype.user
+    redirect_to root_path unless current_user == @prototype.user
   end
 
   def move_to_index
